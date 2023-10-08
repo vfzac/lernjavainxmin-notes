506a090c31fbb7ef77f0d63a6034fcb41f92485d
vfz <thefritzvalzado@gmail.com>
2023-10-09 04:46:12 +0800

    mini session two

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,33 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 
+import java.util.Scanner;
 public class Main {
 
         public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
+            //user input
+            Scanner scanner = new Scanner(System.in);
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
+            //console output
+            System.out.print("Hello and welcome!");
+
+            System.out.println("Integer: " + 10 +
+                               " Double: " + 3.14 +
+                               " Boolean: " + true);
+
+            // Use System.out.printf() for easy formatted printing.
+            System.out.printf("pi = %.5f", Math.PI); // => pi = 3.14159
+
+            // read string input = scanner.next();
+            // read byte input = scanner.nextByte();
+            // read int input = scanner.nextInt();
+            // read long input = scanner.nextLong();
+            // read float input = scanner.nextFloat();
+            // read double input = scanner.nextDouble();
+            // read boolean input = scanner.nextBoolean();
+
+        System.out.print("\n");
         for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
             System.out.println("i = " + i);
         }
     }
